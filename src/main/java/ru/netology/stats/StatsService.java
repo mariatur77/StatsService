2b9c5340cb600ef92dce489890ea18b6e6a9c00c
Maria <mariatur776@gmail.com>
2023-04-04 14:32:25 +0300

    Initial commit

---
+++ src/main/java/ru/netology/stats/StatsService.java
@@ -0,0 +1,67 @@
+package ru.netology.stats;
+
+public class StatsService {
+
+    public long sum(long[] sales) {
+        long totalSale = 0;
+        for (long sale : sales) {
+            totalSale += sale;
+        }
+        return totalSale;
+    }
+
+    public long average(long[] sales) {
+        return sum(sales) / sales.length;
+    }
+
+    public int monthMaximumSale(long[] sales) {
+        int monthMaximum = 0;
+        long saleMaximum = sales[0];
+
+        for (int i = 0; i < sales.length; i++) {
+            if (sales[i] >= saleMaximum) {
+                monthMaximum = i;
+                saleMaximum = sales[i];
+
+            }
+        }
+        return monthMaximum + 1;
+    }
+
+    public int monthMinimumSale(long[] sales) {
+        int monthMinimum = 0;
+        long saleMinimum = sales[0];
+
+        for (int i = 0; i < sales.length; i++) {
+            if (sales[i] <= saleMinimum) {
+                monthMinimum = i;
+                saleMinimum = sales[i];
+
+            }
+        }
+        return monthMinimum + 1;
+    }
+
+    public int monthsBelowAverage(long[] sales) {
+        int counter = 0;
+        for (long sale : sales) {
+            if (sale < average(sales)) {
+                counter++;
+            }
+        }
+        return counter;
+    }
+
+    public int monthsAboveAverage(long[] sales) {
+        int counter = 0;
+        for (long sale : sales) {
+            if (sale > average(sales)) {
+                counter++;
+            }
+        }
+        return counter;
+    }
+}
+
+
+
